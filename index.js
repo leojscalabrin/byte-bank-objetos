d01d13cf3cf68c41b96404a4e56bfb0862a4cf6a
Leonardo Scalabrin <leojscalabrin@gmail.com>
2022-01-09 21:43:53 -0300

    abstração

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Cliente{
     nome;
     cpf;
@@ -44,33 +43,3 @@ const valorSacado = contaCorrenteRicardo.sacar(50);
 console.log(valorSacado);
 
 console.log(contaCorrenteRicardo);
-=======
-class Cliente{
-    nome;
-    cpf;
-    agencia;
-    saldo;
- 
-}
-
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
-
-
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
-
-
-const cliente3Nome = "Alice";
-const cliente3CPF = 88822233309;
-const cliente3Agencia = 1001;
-const cliente3Saldo = 0;
-console.log(cliente1, cliente2);
->>>>>>> 714e586d25e529941b61b784eed0ec26889dfb3e
